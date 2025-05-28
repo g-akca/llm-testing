cb11977320e8aee8ce8c000a764acddc5cabd913
Necip Baha Sağıroğlu <sagiroglun22@itu.edu.tr>
2025-05-28 19:30:28 +0300

    fix

--- phase-2/medium/eval-6/human/test.py
+++ phase-2/medium/eval-6/human/test.py
@@ -21,6 +21,9 @@ class TestParseNestedParens(unittest.TestCase):
     def test_empty_string(self):
         self.assertEqual(parse_nested_parens(""), [])
 
+    def test_only_spaces(self):
+        self.assertEqual(parse_nested_parens("  "), [])
+
     def test_single_paren(self):
         self.assertEqual(parse_nested_parens("()"), [1])
 
@@ -29,6 +32,9 @@ class TestParseNestedParens(unittest.TestCase):
 
     def test_complex(self):
         self.assertEqual(parse_nested_parens("() ((())) (()) (((()(()))))"), [1, 3, 2, 5])
+
+    def test_unnecessary_spaces(self):
+        self.assertEqual(parse_nested_parens("  (()())   ((()))  "), [2, 3])
 
 if __name__ == "__main__":
     unittest.main()
